ebf6af521dcbc6e57c8e71daaa0df2efa3d7ac19
Volha <kresiko@gmail.com>
2021-09-06 19:03:07 +0300

    After second review

--- utils-1.3.5/src/main/java/com/volha/tasks/examples/StringUtils.java
+++ utils-1.3.5/src/main/java/com/volha/tasks/examples/StringUtils.java
@@ -3,17 +3,15 @@ package com.volha.tasks.examples;
 import static org.apache.commons.lang3.StringUtils.isNumeric;
 
 public class StringUtils {
-   public static boolean isPositiveNumber(String str){
-       try {
-           if (isNumeric(str)) {
-               if (Long.parseLong(str) > 0) {
-                   return true;
-               }
-           }
-       }
-       catch (NumberFormatException e){
+
+   public static boolean isPositiveNumber(String str) {
+       if (str==null||str==""){
            return false;
        }
-       return false;
+       try {
+           return isNumeric(str) && (Long.parseLong(str) > 0);
+       } catch (NumberFormatException e) {
+           return false;
+       }
    }
 }
